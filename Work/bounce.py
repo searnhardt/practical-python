c44720e89fabdbeabba15976ad9ff4c47e56305e
Earnhardt <Sean@EuropaSports.com>
2021-03-31 18:26:54 -0400

    Signed-off-by: Earnhardt <Sean@EuropaSports.com>

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100 #initial height of the drop in meters
+bounce = 3/5 #rebound bounce height of each drop
+#What is the height of the bounce for the first 10 bounces?
+
+bounce_num = 1
+while bounce_num < 11:
+    print(bounce_num, " ",round((height * bounce), 4))
+    height=height*bounce
+    bounce_num += 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,29 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment_start_month = int(input('Extra Payment Start Month?'))
+extra_payment_end_month = int(input('Extra Payment End Month?'))
+extra_payment = int(input('Extra Payment Amount Per Month?')) # per month
+months_total = 0
+
+while principal > 0:
+    while months_total >= extra_payment_start_month and months_total <= extra_payment_end_month:
+        principal = principal * (1+rate/12) - payment - extra_payment
+        #principal = principal - extra_payment
+        total_paid = total_paid + payment + extra_payment
+        months_total += 1
+        print((months_total-1), total_paid, principal)
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    months_total += 1
+    if principal < 0:
+        principal -= principal
+    print((months_total-1), total_paid, principal)
+
+print('Total paid', total_paid)
+print('Total Number of Months', months_total)
+
