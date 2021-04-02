3c9d939c8312bb660c06e58c60cdea5ed37508e5
Earnhardt <Sean@EuropaSports.com>
2021-04-02 19:29:35 -0400

    Signed-off-by: Earnhardt <Sean@EuropaSports.com>

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,28 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+portfolio = []
+
+def read_portfolio(filename):
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        try:
+            for row in rows:
+                holding = (row[0], int(row[1]), float(row[2]))
+                portfolio.append(holding)
+        except:
+            print('Bad line found and skipped')
+            next(f)
+    return portfolio
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Work/Data/portfolio.csv'
+
+cost = read_portfolio(filename)
+print('Total cost', cost)
